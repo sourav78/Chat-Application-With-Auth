2de5828209749373848bd341ca174cde56463a3d
sourav78 <souravranjan488@gmail.com>
2024-01-18 18:43:24 +0530

    project initialization

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+
+    return (
+        <>
+            <h1 className='text-3xl text-orange-500'>Hello World</h1>
+        </>
+    )
+}
+
+export default App
