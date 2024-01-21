5ff73b5ecfd5c37d8bf2265709fcf5a278f9c9e6
sourav78 <souravranjan488@gmail.com>
2024-01-21 21:01:10 +0530

    signin and signup page create on frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,21 @@
-import { useState } from 'react'
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Signin from './pages/signin'
+import Signup from './pages/signup'
 
-function App() {
-
+const App = () => {
     return (
         <>
-            <h1 className='text-3xl text-orange-500'>Hello World</h1>
+            <div>
+                <Router>
+                    <Routes>
+                        <Route path='/' element={<Home/>}/>
+                        <Route path='/signin' element={<Signin/>}/>
+                        <Route path='/signup' element={<Signup/>}/>
+                    </Routes>
+                </Router>
+            </div>
         </>
     )
 }
