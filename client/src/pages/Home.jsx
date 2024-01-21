3db1daa9d4fc1c9adaa6447c7f3f5c498dbb701a
sourav78 <souravranjan488@gmail.com>
2024-01-21 21:41:19 +0530

    home page authentication setup frontend

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import axios from 'axios'
 
 const Home = () => {
+
+    useEffect(() => {
+
+        const fetchData = async () => {
+            try {
+                const response = await axios.get("http://localhost:3000", {
+                    withCredentials: true
+                })
+
+                const data = await response.data
+
+                if (response.status !== 200) {
+                    console.log("Login first");
+                }
+                console.log(response);
+                console.log(data);
+            } catch (error) {
+                console.log(error.message);
+            }
+        }
+
+        fetchData()
+    })
+
     return (
         <>
-            
+
         </>
     )
 }
--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const cookieParser = require('cookie-parser')
+const cors = require('cors')
 
 const connectDb = require('./config/db')
 const userRouter = require('./routes/user.routes')
@@ -9,6 +10,11 @@ const userRouter = require('./routes/user.routes')
 const app = express()
 
 app.use(express.json())
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+    credentials: true
+}))
 app.use(cookieParser())
 
 connectDb()
